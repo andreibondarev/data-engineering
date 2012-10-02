4a2c28571cab327312b766d8e576adeb0cc97797
Andrei Bondarev <andreibondarev@Andreis-MacBook-Pro.local>
2012-10-01 22:12:54 -0400

    Initial blank application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DataEngineering::Application.config.secret_token = '9b5126c910eb342d78cd94d17fd0a63bca33f2fc6b8d6825c57193e06786504cf88a2b1c2ff35537cf3fafbc94ce3d5544d29c6988910470c3043d81df3a1031'
